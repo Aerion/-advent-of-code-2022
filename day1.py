ef4976a80ab4074f451809d9c4a37eb141d6a733
Guillaume Taquet Gasperini <g.taquetgasperini@criteo.com>
2022-12-01 09:30:29 +0100

    add day1 - part2

--- day1.py
+++ day1.py
@@ -3,15 +3,18 @@
 import sys
 
 cur_elf_calories = 0
-max_elf_calories = 0
+top_3_elf_calories = [0, 0, 0]
 while line := sys.stdin.readline():
     line = line.strip()
     if not line:
-        max_elf_calories = max(cur_elf_calories, max_elf_calories)
+        if cur_elf_calories > top_3_elf_calories[0]:
+            top_3_elf_calories = [cur_elf_calories] + top_3_elf_calories[1:]
+            top_3_elf_calories.sort()
+
         cur_elf_calories = 0
         continue
 
     value = int(line)
     cur_elf_calories += value
 
-print(max_elf_calories)
+print(sum(top_3_elf_calories))
