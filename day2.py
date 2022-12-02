3a40c5ff534ec67757809693327f0dee70b35505
Guillaume Taquet Gasperini <g.taquetgasperini@criteo.com>
2022-12-02 09:05:40 +0100

    add day2 - part2

--- day2.py
+++ day2.py
@@ -8,6 +8,12 @@ class Move(Enum):
     SCISSORS = 3
 
 
+class Result(Enum):
+    LOSE = 1
+    DRAW = 2
+    WIN = 3
+
+
 def get_winning_score(self_move: Move, opponent_move: Move):
     return {
         (Move.ROCK, Move.ROCK): 3,
@@ -22,12 +28,30 @@ def get_winning_score(self_move: Move, opponent_move: Move):
     }[self_move, opponent_move]
 
 
+def get_wished_move(opponent_move: Move, wished_result: Result):
+    if wished_result == Result.DRAW:
+        return opponent_move
+    if wished_result == Result.LOSE:
+        return {
+            Move.ROCK: Move.SCISSORS,
+            Move.SCISSORS: Move.PAPER,
+            Move.PAPER: Move.ROCK,
+        }[opponent_move]
+    return {
+        Move.ROCK: Move.PAPER,
+        Move.SCISSORS: Move.ROCK,
+        Move.PAPER: Move.SCISSORS,
+    }[opponent_move]
+
+
 total_score = 0
 while line := sys.stdin.readline():
     line = line.strip()
 
     opponent_move = {"A": Move.ROCK, "B": Move.PAPER, "C": Move.SCISSORS}[line[0]]
-    self_move = {"X": Move.ROCK, "Y": Move.PAPER, "Z": Move.SCISSORS}[line[2]]
+    wished_result = {"X": Result.LOSE, "Y": Result.DRAW, "Z": Result.WIN}[line[2]]
+
+    self_move = get_wished_move(opponent_move, wished_result)
 
     total_score += self_move.value + get_winning_score(self_move, opponent_move)
 
