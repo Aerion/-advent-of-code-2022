9c6c50b24ebd1ad02fab14c2a9b1b7a8b74046bc
Guillaume Taquet Gasperini <perso@aerion.me>
2022-12-03 12:19:35 +0100

    add day3 - part2

--- day3.py
+++ day3.py
@@ -1,21 +1,35 @@
+from collections import defaultdict
 import sys
 
-def compute_priority(char):
-    if char <= 'Z':
-        return ord(char) - ord('A') + 1 + 26
 
-    return ord(char) - ord('a') + 1
+def compute_priority(char):
+    if char <= "Z":
+        return ord(char) - ord("A") + 1 + 26
+
+    return ord(char) - ord("a") + 1
+
 
 priorities_sum = 0
+elf_count = 0
+seen_chars_group = defaultdict(lambda: 0)
 while line := sys.stdin.readline():
+    elf_count += 1
     line = line.strip()
 
-    first_rucksack = line[0 : len(line) // 2]
-    second_rucksack = line[len(line) // 2 :]
-    seen_chars = set(first_rucksack)
-    for char in second_rucksack:
+    seen_chars = set()
+    for char in line:
         if char in seen_chars:
-            priorities_sum += compute_priority(char)
-            break
+            continue
+        seen_chars.add(char)
+        seen_chars_group[char] += 1
+
+    if elf_count == 3:
+        # Last elf of the group
+        for char, count in seen_chars_group.items():
+            if count == 3:
+                priorities_sum += compute_priority(char)
+                elf_count = 0
+                seen_chars_group.clear()
+                break
 
 print(priorities_sum)
