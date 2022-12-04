f3ad3f044f17af8c6188ddd18cec7e8c2d5c2934
Guillaume Taquet Gasperini <perso@aerion.me>
2022-12-04 11:47:38 +0100

    add day4 - part2

--- day4.py
+++ day4.py
@@ -3,12 +3,12 @@ import sys
 total_common = 0
 while line := sys.stdin.readline():
     line = line.strip()
-    pairs = [[int(s) for s in x.split("-")] for x in line.split(",")]
-
-    if (pairs[0][0] <= pairs[1][0] and pairs[0][1] >= pairs[1][1]) or (
-        pairs[1][0] <= pairs[0][0] and pairs[1][1] >= pairs[0][1]
-    ):
+    pairs = [
+        list(range(int(x.split("-")[0]), int(x.split("-")[1]) + 1))
+        for x in line.split(",")
+    ]
+    
+    if set(pairs[0]).intersection(pairs[1]):
         total_common += 1
-        continue
 
 print(total_common)
