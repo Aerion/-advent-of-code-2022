ee4ae88e5c616cb867de1e01d6ecbe3f085ec782
Guillaume Taquet Gasperini <perso@aerion.me>
2022-12-06 07:54:45 +0100

    add day6 - part1

---
+++ day6.py
@@ -0,0 +1,20 @@
+import sys
+
+input = sys.stdin.readline().strip()
+
+MARKER_LENGTH = 4
+last_seen_pos = [-MARKER_LENGTH] * 26
+
+first_noncontinuous_pos = 0
+for pos, char in enumerate(input):
+    val = ord(char) - ord('a')
+
+    last_pos = last_seen_pos[val]
+    if pos - last_pos >= MARKER_LENGTH:
+        if pos - first_noncontinuous_pos == MARKER_LENGTH:
+            print(str(pos))
+            break
+    else:
+        first_noncontinuous_pos = max(last_seen_pos[val] + 1, first_noncontinuous_pos)
+
+    last_seen_pos[val] = pos
