e4ecb8bbb0f50154cca12d92a51e57579783841f
Guillaume Taquet Gasperini <g.taquetgasperini@criteo.com>
2022-12-12 08:11:03 +0100

    add day12 - part1

---
+++ day12.py
@@ -0,0 +1,69 @@
+import sys
+from dataclasses import dataclass
+
+
+@dataclass
+class Node:
+    height: int
+    visited: bool
+
+
+@dataclass
+class Position:
+    x: int
+    y: int
+
+
+start_pos = Position(0, 0)
+end_pos = Position(0, 0)
+
+nodes: list[list[Node]] = []
+y = 0
+while line := sys.stdin.readline().strip():
+    row = []
+    for x, c in enumerate(line):
+        if c == "S":
+            start_pos = Position(x, y)
+            c = "a"
+        elif c == "E":
+            end_pos = Position(x, y)
+            c = "z"
+        row.append(Node(ord(c) - ord("a"), False))
+    nodes.append(row)
+    y += 1
+
+q = [(start_pos, 0)]
+
+print(f"Target: {end_pos}")
+
+while q:
+    (pos, steps) = q.pop(0)
+    if pos.y == end_pos.y and pos.x == end_pos.x:
+        print(steps)
+        break
+
+    print(steps, pos)
+    node = nodes[pos.y][pos.x]
+    if node.visited:
+        continue
+
+    node.visited = True
+
+    # Add neighbors
+    def add_candidate(new_pos: Position):
+        if (
+            new_pos.y >= len(nodes)
+            or new_pos.x >= len(nodes[0])
+            or new_pos.y < 0
+            or new_pos.x < 0
+        ):
+            return
+        candidate = nodes[new_pos.y][new_pos.x]
+        if candidate.visited or candidate.height > node.height + 1:
+            return
+        q.append((new_pos, steps + 1))
+
+    add_candidate(Position(pos.x + 1, pos.y))
+    add_candidate(Position(pos.x - 1, pos.y))
+    add_candidate(Position(pos.x, pos.y + 1))
+    add_candidate(Position(pos.x, pos.y - 1))
