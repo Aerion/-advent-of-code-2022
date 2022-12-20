79a4e65186e01aea352665f733d374d4fd41cec9
Guillaume Taquet Gasperini <perso@aerion.me>
2022-12-20 19:15:56 +0100

    add day18 - part2

--- day18.py
+++ day18.py
@@ -3,12 +3,16 @@ import sys
 from dataclasses import dataclass
 from typing import Optional
 
+sys.setrecursionlimit(10_000_000)
+
 
 @dataclass
 class Droplet:
     x: int
     y: int
     z: int
+
+all_droplets: list[Droplet] = []
 
 
 class Grid:
@@ -20,6 +24,7 @@ class Grid:
     def add_droplet(self, x: int, y: int, z: int):
         droplets_z = self._get_droplets_z(x, y)
         droplets_z[z] = Droplet(x, y, z)
+        all_droplets.append(droplets_z[z])
 
     def get_droplet(self, x: int, y: int, z: int):
         droplets_z = self._get_droplets_z(x, y)
@@ -37,23 +42,71 @@ grid = Grid()
 
 adjacent_sides = 0
 working_side = 0
+min_z = None
+max_z = None
+min_x = None
+max_x = None
+min_y = None
+max_y = None
 while line := sys.stdin.readline().rstrip():
     x, y, z = (int(part) for part in line.split(","))
     grid.add_droplet(x, y, z)
+    min_z = min(z, min_z or z)
+    max_z = max(z, max_z or z)
+    min_y = min(y, min_z or y)
+    min_x = min(x, min_x or x)
+    max_y = max(y, max_y or y)
+    max_x = max(x, max_x or x)
 
-    for (addx, addy, addz) in [
-        (0, 0, 1),
-        (0, 0, -1),
-        (0, 1, 0),
-        (0, -1, 0),
-        (-1, 0, 0),
-        (1, 0, 0),
+min_x -= 3
+min_y -= 3
+min_z -= 3
+max_x += 3
+max_y += 3
+max_z += 3
+
+edges_count = 0
+
+
+def is_in_bounds(x: int, y: int, z: int):
+    return (
+        x >= min_x
+        and y >= min_y
+        and z >= min_z
+        and x < max_x
+        and y < max_y
+        and z < max_z
+    )
+
+
+visited_nodes: set[tuple[int, int, int]] = set()
+
+
+def dfs(x: int, y: int, z: int):
+    # print(x, y, z)
+    edges_count = 0
+
+    visited_nodes.add((x, y, z))
+    for (addx, addy, addz, side) in [
+        (0, 0, 1, "ahead"),
+        (0, 0, -1, "behind"),
+        (0, 1, 0, "top"),
+        (0, -1, 0, "bottom"),
+        (-1, 0, 0, "left"),
+        (1, 0, 0, "right"),
     ]:
-        if grid.get_droplet(x + addx, y + addy, z + addz):
-            adjacent_sides += 1
-            working_side -= 1
-        else:
-            working_side += 1
+        if (x + addx, y + addy, z + addz) in visited_nodes or not is_in_bounds(x, y, z):
+            continue
 
-print(adjacent_sides)
-print(working_side)
+        droplet = grid.get_droplet(x + addx, y + addy, z + addz)
+        if droplet:
+            edges_count += 1
+
+        if not droplet:
+            edges_count += dfs(x + addx, y + addy, z + addz)
+            continue
+
+    return edges_count
+
+
+print(dfs(min_x, min_y, min_z))
