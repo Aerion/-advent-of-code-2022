d48f0a3e7e42d0d411955a1cba0061d012f06c03
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-01 06:26:05 +0100

    add template

---
+++ 2024/template.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from aocd import puzzle
+
+EXAMPLE_IDX = 0
+
+data = (puzzle.examples[EXAMPLE_IDX] if EXAMPLE_IDX is not None else puzzle).input_data
+
+print(f"Puzzle #{puzzle.day}")
+
+if EXAMPLE_IDX is not None:
+    print(f"Using example #{EXAMPLE_IDX} data")
+else:
+    print(f"Using PROD data")
+
+result = 0
+for line in data.splitlines():
+    pass
+
+print(f"Result: {result}")
