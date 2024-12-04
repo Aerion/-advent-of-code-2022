5f4a30969c842f60c376c9fdbcbc2b248b9cb00f
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-04 06:39:53 +0100

    add day4 - part2

--- 2024/day4.py
+++ 2024/day4.py
@@ -1,32 +1,10 @@
 #!/usr/bin/env python
 
 from aocd import puzzle
-from enum import Enum
-
-class Direction(Enum):
-    LEFT = 1
-    RIGHT = 2
-    TOP = 3
-    BOTTOM = 4
-    BOTTOM_LEFT = 5
-    BOTTOM_RIGHT = 6
-    TOP_LEFT = 7
-    TOP_RIGHT = 8
-
-DIRECTION_VECTORS = {
-    Direction.LEFT: (-1, 0),
-    Direction.RIGHT: (1, 0),
-    Direction.TOP: (0, -1),
-    Direction.BOTTOM: (0, 1),
-    Direction.BOTTOM_LEFT: (1, -1),
-    Direction.BOTTOM_RIGHT: (1, +1),
-    Direction.TOP_LEFT: (-1, -1),
-    Direction.TOP_RIGHT: (-1, 1),
-}
 
 EXAMPLE_IDX = None
 
-VALID_WORD = "XMAS"
+VALID_WORD = "MAS"
 VALID_CHARS = set(VALID_WORD)
 
 data = (puzzle.examples[EXAMPLE_IDX] if EXAMPLE_IDX is not None else puzzle).input_data
@@ -47,33 +25,24 @@ for line in data.splitlines():
 width = len(map[0])
 height = len(map)
 
-from pprint import pprint
-pprint(map)
-
-def search_xmas(x, y, valid_length, direction: Direction):
-    # print(x, y, valid_length)
+def try_get_pos(x, y):
     if y < 0 or y >= height or x < 0 or x >= width:
-        return 0
-    if map[y][x] != VALID_WORD[valid_length]:
-        return 0
-
-    new_valid_length = valid_length + 1
-    if new_valid_length == len(VALID_WORD):
-        print(x, y, map[y][x], valid_length, VALID_WORD[valid_length])
-        # Found it
-        return 1
+        return False
     
-    # Still on the search
-    inc_x, inc_y = DIRECTION_VECTORS[direction]
-    return search_xmas(x + inc_x, y + inc_y, new_valid_length, direction)
+    return map[y][x]
 
+VALID_MAS_SET = set(('M', 'S'))
+
+def is_valid_a(x, y):
+    return (try_get_pos(x, y) == 'A' 
+            and set((try_get_pos(x - 1, y - 1), try_get_pos(x + 1, y + 1))) == VALID_MAS_SET
+            and set((try_get_pos(x + 1, y - 1), try_get_pos(x - 1, y + 1))) == VALID_MAS_SET
+    )
 
 result = 0
-
 for y in range(height):
     for x in range(width):
-        for direction in Direction:
-            result += search_xmas(x, y, 0, direction)
-
+        if is_valid_a(x, y):
+            result += 1
 
 print(f"Result: {result}")
