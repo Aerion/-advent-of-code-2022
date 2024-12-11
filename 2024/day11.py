f091f4261efd8f7dc6bb7c571e7b5b8d10727a9c
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-11 06:34:06 +0100

    add day11 - part2

--- 2024/day11.py
+++ 2024/day11.py
@@ -15,26 +15,50 @@ if EXAMPLE_IDX is not None:
 else:
     print(f"Using PROD data")
 
-stones = [int(x) for x in data.split()]
-for i in range(25):
-    new_stones = []
-    for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
-        elif len(str(stone)) % 2 == 0:
-            # FAR find better way
-            l = len(str(stone))
-            #print(stone)
-            left = int(str(stone)[0:l//2])
-            right = int(str(stone)[l//2:])
-            new_stones.append(left)
-            new_stones.append(right)
-        else:
-            new_stones.append(stone * 2024)
-    stones = new_stones
+def brute_get_stones_after_n_rounds(stones: list[int], rounds_count: int):
+    for j in range(rounds_count):
+            new_stones = []
+            for stone in stones:
+                if stone == 0:
+                    new_stones.append(1)
+                elif len(str(stone)) % 2 == 0:
+                    # FAR find better way
+                    l = len(str(stone))
+                    #print(stone)
+                    left = int(str(stone)[0:l//2])
+                    right = int(str(stone)[l//2:])
+                    new_stones.append(left)
+                    new_stones.append(right)
+                else:
+                    new_stones.append(stone * 2024)
+            stones = new_stones
+    return stones
+
+future_round_by_stone = {}
+#for i in list(range(10)) + [2024]:
+    #stones = [i]
+    #future_round_by_stone[i] = brute_get_stones_after_n_rounds(stones, 6)
+
+print(future_round_by_stone)
+
+stones_count = defaultdict(int)
+for stone in [int(x) for x in data.split()]:
+    stones_count[stone] += 1
+
+for i in range(0, 75, 5):
+    print(i, stones_count)
+
+    new_stones_count = defaultdict(int)
+    for stone, count in stones_count.items():
+        if stone not in future_round_by_stone:
+            future_round_by_stone[stone] = brute_get_stones_after_n_rounds([stone], 5)
+        for new_stone in future_round_by_stone[stone]:
+            new_stones_count[new_stone] += count
+        
+    stones_count = new_stones_count
     #print(stones)
 
-result = len(stones)
+result = sum(stones_count.values())
 
 print(f"Result: {result}")
 if EXAMPLE_IDX is None:
