1472b63d8eaa85f1a058bb03905061e4d77c5969
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-12 08:14:31 +0100

    [WIP] day12 - part2

--- 2024/day12.py
+++ 2024/day12.py
@@ -3,7 +3,7 @@
 from aocd import puzzle, submit
 from collections import defaultdict
 
-EXAMPLE_IDX = None
+EXAMPLE_IDX = 0
 
 data = (puzzle.examples[EXAMPLE_IDX] if EXAMPLE_IDX is not None else puzzle).input_data
 
@@ -34,27 +34,54 @@ else:
 def in_bounds(x: int, y: int, map: list[list[str]]):
     return x >= 0 and y >= 0 and x < len(map[0]) and y < len(map)
 
-def get_zone_score(x: int, y: int, map: list[list[str]], plot: str, visited: set):
+
+FENCE_BY_DIRECTION = {
+    (1, 0): '}',
+    (-1, 0): '{',
+    (0, 1): '_',
+    (0, -1): '-',
+}
+
+OPPOSITE_DIRECTION = {
+    (1, 0): (-1, 0),
+    (-1, 0): (1, 0),
+    (0, 1): (0, -1),
+    (0, -1) : (0, 1)
+}
+
+def get_zone_score(x: int, y: int, map: list[list[str]], plot: str, visited: set, direction: tuple[int, int], fences: dict[tuple[int, int], set[str]]):
+    #print(x,y)
     if not in_bounds(x, y, map) or map[y][x] != plot:
         # Increase perimeter by 1
         return 0, 1
     
     # Vist the node
-    map[y][x] = '.'
-    visited.add((x, y))
-    
+    #map[y][x] = '.'
+    visited.add((x, y, direction))
+
     # Increase area by 1
     area, perimeter = 1, 0
     
-    for new_x, new_y in ((x + 1, y), (x - 1, y), (x, y + 1), (x, y - 1)):
-        if (new_x, new_y) in visited:
+    for new_direction in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+        fences[x, y].add(FENCE_BY_DIRECTION[new_direction])
+
+        new_x, new_y = (new_direction[0] + x, new_direction[1] + y)
+        if (new_x, new_y, new_direction) in visited:
             continue
-        area_delta, perimeter_delta = get_zone_score(new_x, new_y, map, plot, visited)
+        area_delta, perimeter_delta = get_zone_score(new_x, new_y, map, plot, visited, new_direction, fences)
         area += area_delta
         perimeter += perimeter_delta
+
+        print(f"Checking {new_x} {new_y} from {x} {y}")
+        if area_delta > 0:
+            # We found another plot, remove the fence
+            print(f"Found another plot, remove {FENCE_BY_DIRECTION[new_direction]} from {x} {y} (with existing {fences[x, y]}) and remove {FENCE_BY_DIRECTION[OPPOSITE_DIRECTION[new_direction]]} from {new_x} {new_y}")
+            if FENCE_BY_DIRECTION[new_direction] in fences[x, y]:
+                fences[x, y].remove(FENCE_BY_DIRECTION[new_direction])
+            if FENCE_BY_DIRECTION[OPPOSITE_DIRECTION[new_direction]] in fences[new_x, new_y]:
+                fences[new_x, new_y].remove(FENCE_BY_DIRECTION[OPPOSITE_DIRECTION[new_direction]])
     
     return area, perimeter
-    
 
 map = [[x for x in line] for line in data.splitlines()]
 
@@ -68,10 +95,44 @@ for y, line in enumerate(map):
         if plot == '.':
             continue
         visited = set()
-        area, perimeter = get_zone_score(x, y, map, plot, visited)
+        fences = defaultdict(set)
+        area, perimeter = get_zone_score(x, y, map, plot, visited, (1, 0), fences)
         result += area * perimeter
-        print(area, perimeter, result)
-        print_map()
+
+        #print(area, perimeter, result)
+        print(fences)
+        #print_map()
+
+        positions_by_fence_kind: dict[dict[list[int], int], str] = {}
+        for pos in fences:
+            for fence in fences[pos]:
+                if fence not in positions_by_fence_kind:
+                    positions_by_fence_kind[fence] = defaultdict(list[int])
+                if fence == '}' or fence == '{':
+                    key, value = pos[0], pos[1]
+                else:
+                    key, value = pos[1], pos[0]
+                positions_by_fence_kind[fence][key].append(value)
+
+        print(positions_by_fence_kind)
+
+        total_sides_count = 0
+        for fence_kind, candidates_group in positions_by_fence_kind.items():
+            sides_count = 0
+
+            for candidates in candidates_group.values():
+                fence_sides_count = 1
+                candidates.sort()
+                print(candidates)
+                for i in range(1, len(candidates)):
+                    if candidates[i - 1] + 1 != candidates[i]:
+                        fence_sides_count += 1
+                sides_count += fence_sides_count
+            print(f"{fence_kind} has {sides_count} sides for plot {plot}")
+            total_sides_count += sides_count
+
+        print(f"Plot {plot} has {total_sides_count} sides")
+
 
 print(f"Result: {result}")
 if EXAMPLE_IDX is None and data == puzzle.input_data:
