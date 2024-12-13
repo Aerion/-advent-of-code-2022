7b824231367989c12f883345e14a4d2ac4802832
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-13 07:30:33 +0100

    add day13 - part2

--- 2024/day13.py
+++ 2024/day13.py
@@ -18,27 +18,27 @@ else:
 
 
 def solve_equation(ax, ay, bx, by, xtarget, ytarget):
-    def is_valid(a, b):
-        #print(f"{a=} {b=} {ax * a + bx * b == xtarget=} and {ay * a + by * b == ytarget=}")
-        #print(f"{a=} {b=} {ax} * {a} + {bx} * {b} == {xtarget} and {ay} * {a} + {by} * {b} == {ytarget}")
-        return ax * a + bx * b == xtarget and ay * a + by * b == ytarget
-    
-    for i in range(100):
-        for j in range(100):
-            if is_valid(i, j):
-                return i, j
-    
-    return None, None
+    # Solve 
+    #   ax * a + bx * b == xtarget
+    #   ay * a + by * b == ytarget
+
+    b = (-xtarget * ay + ytarget * ax) / (-bx * ay + by * ax)
+    a = (xtarget - bx * b) / ax
+
+    return a,b
 
 result = 0
 for i in range(0, len(data), 4):
     a_increment = map(int, [formula.split('+')[1] for formula in data[i].split(',')])
     b_increment = map(int, [formula.split('+')[1] for formula in data[i + 1].split(',')])
-    target = map(int, [formula.split('=')[1] for formula in data[i + 2].split(',')])
+    target = map(lambda x: 10000000000000 + x, map(int, [formula.split('=')[1] for formula in data[i + 2].split(',')]))
 
     a_res, b_res = solve_equation(*a_increment, *b_increment, *target)
-    if a_res is not None:
+    if round(a_res) == a_res and round(b_res) == b_res:
+        print(f"Round: {a_res} {b_res}")
         result += a_res * 3 + b_res
+    else:
+        print(f"Not round: {a_res} {b_res}")
 
 print(f"Result: {result}")
 if EXAMPLE_IDX is None:
