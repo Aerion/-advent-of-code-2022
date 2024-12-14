1833646486f756c5358ed32ab9e1ce583dad8ca4
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-14 07:20:20 +0100

    add day14 - part2

--- 2024/day14.py
+++ 2024/day14.py
@@ -6,12 +6,8 @@ from collections import defaultdict
 EXAMPLE_IDX = None
 HEIGHT = 103
 WIDTH = 101
-#HEIGHT = 7
-#WIDTH = 11
-DURATION = 100
 
 data = (puzzle.examples[EXAMPLE_IDX] if EXAMPLE_IDX is not None else puzzle).input_data
-#data ="""p=2,4 v=2,-3"""
 
 print(f"Puzzle #{puzzle.day}")
 
@@ -20,28 +16,40 @@ if EXAMPLE_IDX is not None:
 else:
     print(f"Using PROD data")
 
-
-robots_count_by_quadrant = defaultdict(int)
+robots = []
 for line in data.splitlines():
     elms = line.split(' ')
     pos = [int(x) for x in elms[0].split('=')[1].split(',')]
     velocity = [int(x) for x in elms[1].split('=')[1].split(',')]
+    robots.append((pos, velocity))
 
-    #for DURATION in range(6):
-    new_pos = ((pos[0] + (velocity[0] * DURATION) + WIDTH) % WIDTH, (pos[1] + (velocity[1] * DURATION) + HEIGHT) % HEIGHT)
-        #print(pos, velocity, new_pos)
-    if new_pos[0] == (WIDTH - 1) // 2 or new_pos[1] == (HEIGHT - 1) // 2:
-        continue
+def get_new_pos(pos, velocity, duration):
+    return ((pos[0] + (velocity[0] * duration) + WIDTH) % WIDTH, (pos[1] + (velocity[1] * duration) + HEIGHT) % HEIGHT)
+
+for duration in range(99999999999999999):
+    unique_pos = set()
     
-    quadrant_x = 0 if new_pos[0] < ((WIDTH - 1) // 2) else 1
-    quadrant_y = 0 if new_pos[1] < ((HEIGHT - 1) // 2) else 1
-    robots_count_by_quadrant[(quadrant_x, quadrant_y)] += 1
+    for pos, velocity in robots:
+        new_pos = get_new_pos(pos, velocity, duration)
+        if 20 < new_pos[1] < 40:
+            unique_pos.add(new_pos)
 
-print(robots_count_by_quadrant)
-result = 1
-for robots_count in robots_count_by_quadrant.values():
-    result *= robots_count
+    if len(unique_pos) > .3 * len(robots):
+        table = []
+        for n in range(HEIGHT):
+            table += [['.'] * WIDTH]
+        for pos, velocity in robots:
+            new_pos = get_new_pos(pos, velocity, duration)
+            table[new_pos[1]][new_pos[0]] = '#'
+        print(duration, len(unique_pos))
+        for n in range(HEIGHT):
+            print("".join(table[n]))
+        print("\n\n\n\n\n")
 
+        from time import sleep
+        sleep(0.05)
+
+"""
 print(f"Result: {result}")
 if EXAMPLE_IDX is None:
-    submit(result)
+    submit(result)"""
