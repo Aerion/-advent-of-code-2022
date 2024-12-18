212f80efc7479e51e05a07d6af5c09c2b844b943
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-18 06:43:29 +0100

    add day18 - part2

--- 2024/day18.py
+++ 2024/day18.py
@@ -5,7 +5,6 @@ from collections import defaultdict, deque
 
 EXAMPLE_IDX = None
 WIDTH = 70 + 1
-ALWAYS_TIME = 1024 - 1
 
 data = (puzzle.examples[EXAMPLE_IDX] if EXAMPLE_IDX is not None else puzzle).input_data
 
@@ -24,7 +23,6 @@ for time, line in enumerate(data.splitlines()):
         corrupted_times[(x, y)] = time
 
 def is_corrupted(x, y, time, corrupted_time):
-    time = ALWAYS_TIME # override
     corrupted_at = corrupted_time.get((x, y), 99999999999999999999999999)
     return time >= corrupted_at
 
@@ -41,30 +39,40 @@ def print_map(time, corrupted_times, path):
         print()
 
 print(corrupted_times)
-result = 0
-q = deque()
-q.append((0, 0, 0, set(), []))
-while q:
-    x, y, time, visited, path = q.popleft()
-    #print(x, y, time)
-    #print_map(time, corrupted_times, path)
+result = None
 
-    if x == WIDTH - 1 and y == WIDTH - 1:
-        result = time
+for i in range(1024, len(data.splitlines())):
+    valid_path = False
+    #print("==================")
+    q = deque()
+    q.append((0, 0, 0, set(), []))
+    while q:
+        x, y, time, visited, path = q.popleft()
+        #print(x, y, time)
+        #print_map(time, corrupted_times, path)
+
+        if x == WIDTH - 1 and y == WIDTH - 1:
+            valid_path = True
+            break
+
+        if x < 0 or y < 0 or x >= WIDTH or y >= WIDTH or (x, y) in visited:
+            continue
+        if is_corrupted(x, y, i, corrupted_times):
+            continue
+
+        visited.add((x, y))
+        new_path = path + [(x,y)]
+
+        q.append((x + 1, y, time + 1, visited, new_path))
+        q.append((x - 1, y, time + 1, visited, new_path))
+        q.append((x, y + 1, time + 1, visited, new_path))
+        q.append((x, y - 1, time + 1, visited, new_path))
+
+    if not valid_path:
+        result_pos = next((pos for pos, time in corrupted_times.items() if time == i))
+        print(time, result_pos)
+        result = f"{result_pos[0]},{result_pos[1]}"
         break
-
-    if x < 0 or y < 0 or x >= WIDTH or y >= WIDTH or (x, y) in visited:
-        continue
-    if is_corrupted(x, y, time, corrupted_times):
-        continue
-
-    visited.add((x, y))
-    new_path = path + [(x,y)]
-
-    q.append((x + 1, y, time + 1, visited, new_path))
-    q.append((x - 1, y, time + 1, visited, new_path))
-    q.append((x, y + 1, time + 1, visited, new_path))
-    q.append((x, y - 1, time + 1, visited, new_path))
 
 print(f"Result: {result}")
 if EXAMPLE_IDX is None and data == puzzle.input_data:
