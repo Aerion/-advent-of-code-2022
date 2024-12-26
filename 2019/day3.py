3657833061e6b6a22d210680f7333e4aa6310465
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-26 09:13:42 +0100

    2019: day3: add part2

--- 2019/day3.py
+++ 2019/day3.py
@@ -46,17 +46,25 @@ directions = {
 
 wires = data.splitlines()
 map: dict[tuple[int, int], int] = defaultdict(int)
+wire_step_by_pos = [
+    {}, {}
+]
 min_x, min_y, max_x, max_y = 0, 0, 0, 0
 for wire_idx, wire in enumerate(wires):
     x, y = 0, 0
+    step = 0
     for instruction in wire.split(","):
         length = int(instruction[1:])
         direction = directions[instruction[0]]
 
         for i in range(length):
+            step += 1
             x += direction[0]
             y += direction[1]
+
             map[(x, y)] |= 1 << wire_idx
+            if (x, y) not in wire_step_by_pos[wire_idx]:
+                wire_step_by_pos[wire_idx][(x, y)] = step
 
             min_x = min(min_x, x)
             max_x = max(max_x, x)
@@ -69,7 +77,7 @@ if EXAMPLE_IDX is not None:
 min_distance = 2 ** 32
 for key, value in map.items():
     if value == 3:
-        distance = abs(key[0]) + abs(key[1])
+        distance = wire_step_by_pos[0][key] + wire_step_by_pos[1][key]
         min_distance = min(min_distance, distance)
 
 result = min_distance
