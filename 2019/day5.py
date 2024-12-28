5998a3c46ebd82b0920bccab750f89d8b6ce04cf
Guillaume Taquet Gasperini <perso@aerion.me>
2024-12-28 21:17:07 +0100

    2019: day5: add tests and refacto

--- 2019/day5.py
+++ 2019/day5.py
@@ -73,58 +73,64 @@ def parse_instruction(ip: int, program: list[int]) -> tuple[OpCode, list[int]]:
         parameters.append(param_position)
 
     return op_code, parameters
+
+def run_program(program: list[int], input: int):
+    ip = 0
+    output = []
+    while True:
+        op_code, parameters = parse_instruction(ip, program)
+
+        if op_code == OpCode.HALT:
+            # No need to process anymore if we must halt
+            break
+        
+        ip += 1 + len(parameters)
+
+        if op_code == OpCode.ADD:
+            output_address = parameters[2]
+            program[output_address] = program[parameters[0]] + program[parameters[1]]
+            continue
+        if op_code == OpCode.MULT:
+            output_address = parameters[2]
+            program[output_address] = program[parameters[0]] * program[parameters[1]]
+            continue
+        if op_code == OpCode.WRITE_AT:
+            program[parameters[0]] = input
+            continue
+        if op_code == OpCode.OUTPUT:
+            output.append(program[parameters[0]])
+            continue
+        if op_code == OpCode.JNZ:
+            if program[parameters[0]] != 0:
+                ip = program[parameters[1]]
+            continue
+        if op_code == OpCode.JZ:
+            if program[parameters[0]] == 0:
+                ip = program[parameters[1]]
+            continue
+        if op_code == OpCode.JLT:
+            program[parameters[2]] = 1 if program[parameters[0]] < program[parameters[1]] else 0
+            continue
+        if op_code == OpCode.JEQ:
+            program[parameters[2]] = 1 if program[parameters[0]] == program[parameters[1]] else 0
+            continue
+
+        print(f"[bold red]Unexpected op_code {op_code}[/]")
+        assert False
+    
+    return output
     
 assert parse_instruction(0, [1002, 4, 3, 4, 33]) == (OpCode.MULT, [4, 2, 4])
+test_program = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
+1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+assert run_program(test_program, 7) == [999]
+assert run_program(test_program, 8) == [1000]
+assert run_program(test_program, 9) == [1001]
 
 result = None
 program = [int(x) for x in data.split(",")]
-
-ip = 0
-output = []
-while True:
-    op_code, parameters = parse_instruction(ip, program)
-
-    if op_code == OpCode.HALT:
-        # No need to process anymore if we must halt
-        break
-
-    assert not output or output[-1] == 0 # Each output must be 0
-    
-    ip += 1 + len(parameters)
-
-    if op_code == OpCode.ADD:
-        output_address = parameters[2]
-        program[output_address] = program[parameters[0]] + program[parameters[1]]
-        continue
-    if op_code == OpCode.MULT:
-        output_address = parameters[2]
-        program[output_address] = program[parameters[0]] * program[parameters[1]]
-        continue
-    if op_code == OpCode.WRITE_AT:
-        input = 5
-        program[parameters[0]] = input
-        continue
-    if op_code == OpCode.OUTPUT:
-        output.append(program[parameters[0]])
-        continue
-    if op_code == OpCode.JNZ:
-        if program[parameters[0]] != 0:
-            ip = program[parameters[1]]
-        continue
-    if op_code == OpCode.JZ:
-        if program[parameters[0]] == 0:
-            ip = program[parameters[1]]
-        continue
-    if op_code == OpCode.JLT:
-        program[parameters[2]] = 1 if program[parameters[0]] < program[parameters[1]] else 0
-        continue
-    if op_code == OpCode.JEQ:
-        program[parameters[2]] = 1 if program[parameters[0]] == program[parameters[1]] else 0
-        continue
-
-    print(f"[bold red]Unexpected op_code {op_code}[/]")
-    assert False
-
+output = run_program(program, 5)
 result = output[-1]
 #################################################################
 # No changes after this line
